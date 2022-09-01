828ef9eb6515b41a47d4ca65503d4c62b3756127
alves-andersonj <75147128+alves-andersonj@users.noreply.github.com>
2022-09-01 11:09:43 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    i=0;
+    
+    for (i = 1; i <= 100; ++i)
+    {
+		if (i % 3 == 0)
+			printf("%s\n","FooBaa");
+		else if (i % 5 == 0)
+			printf("%s\n","Baa");
+		else if (i % 15 == 0)
+			printf("%s\n","Foo");
+		else 
+			printf("%d\n",i);
+	}
+}
